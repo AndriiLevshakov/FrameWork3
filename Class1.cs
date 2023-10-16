24ecd2f24b5371d9af3f1aa22cdc55ef6c038cf2
Andrey Levshakov <andreylevshakov@gmail.com>
2023-10-16 13:35:54 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    
+    public class Class1
+    {
+        IWebDriver driver = new ChromeDriver();
+        WebDriverWait wait;
+
+        [Test]
+        public void Test()
+        {
+            driver.Navigate().GoToUrl("https://cloud.google.com/products/calculator");
+            Thread.Sleep(5000);
+            driver.SwitchTo().Frame("myFrame");
+            IWebElement inputField = wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@id='input_98']")));
+            inputField.Click();
+            inputField.SendKeys("98987");
+
+            driver.Quit();
+
+        }
+        
+
+
+
+    }
+}
---
+++ EmailSendingFromUkrNetTest.cs
@@ -0,0 +1,48 @@
+﻿using Core.Configuration;
+using Core.DriverFactory;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class EmailSendingFromUkrNetTest
+    {
+        IWebDriver driver;
+        private UkrNetDesktopPage ukrNetDesktopPage;
+        private UkrNetLoginPage ukrNetLoginPage;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            var browser = (Drivers)Enum.Parse(typeof(Drivers), Configuration.Model.Browser);
+            driver = DriverProvider.GetDriverFactory(browser).CreateDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            ukrNetLoginPage = new UkrNetLoginPage(driver);
+            ukrNetDesktopPage = new UkrNetDesktopPage(driver);
+            ukrNetLoginPage.NavigateToUkrNetLoginPage();            
+        }
+
+        [Test]
+        public void SendEmailTest()
+        {
+            ukrNetLoginPage.LogIn();
+            //Thread.Sleep(5000);
+            ukrNetDesktopPage.SendEmail();
+
+            Assert.IsTrue(ukrNetDesktopPage.ConfirmationCheck());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
